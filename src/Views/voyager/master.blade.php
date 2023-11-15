958c650f4d1b321d2ed93f1758c868961bf0e1bd
hasbullah-mughniy <hasbullachalmughniy@gmail.com>
2023-11-15 10:56:39 +0700

    - Fix compability

--- src/Views/voyager/master.blade.php
+++ src/Views/voyager/master.blade.php
@@ -139,11 +139,11 @@ if (\Illuminate\Support\Str::startsWith(Auth::user()->avatar, 'http://') || \Ill
     }
     @endif
 </script>
-<script>
+<!-- <script>
     $(document).ready(function(){
       $('[data-toggle="tooltip"]').tooltip();
     });
-</script>
+</script> -->
 @include('voyager::media.manager')
 @yield('javascript')
 @stack('javascript')
